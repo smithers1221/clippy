430684b58eae61fcc0bada41b2a4810f92da2ad7
wlaoh <winston@replicated.com>
2017-05-22 11:18:42 -0700

    bookthattruck twitter is really messing with me

--- skills/food_truck.js
+++ skills/food_truck.js
@@ -7,7 +7,7 @@ Food truck controllers for Replicated, Inc.
 module.exports = function(controller) {
 
     controller.hears(['food', 'hungry', 'lunch', 'truck'], 'direct_message,direct_mention,mention', function(bot, message) {
-
+        console.log("HERE")
         controller.storage.users.get(message.user, function(err, user) {
             var Twit = require('twit');
             var moment = require('moment');
@@ -17,7 +17,7 @@ module.exports = function(controller) {
             , access_token:         process.env.ACCESS_TOKEN
             , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
         })
-            T.get('statuses/user_timeline', { count:20, since_id: moment().format("YYYY-MM-DD"), trim_user: true, exclude_replies: true, user_id: 2295568387 }, function(err, data, response) {
+            T.get('statuses/user_timeline', { count:30, since_id: moment().format("YYYY-MM-DD"), trim_user: true, exclude_replies: true, user_id: 2295568387 }, function(err, data, response) {
                 for (var i = 0; i < data.length; i++) {
                     if ((data[i].text.indexOf('@bLAckwelder_LA') != -1) && (data[i].text.indexOf(moment().format('ddd').toUpperCase()) != -1)) {
                         bot.reply(message, 'Hello, today\'s food truck tweet is: ' + data[i].text);
